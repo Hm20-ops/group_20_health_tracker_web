5e2d9b589ea59e4a5a18de485f51254fc0fa9fa3
Hm20-ops <61349319+Hm20-ops@users.noreply.github.com>
2020-10-18 12:08:35 +0100

    Update settings.py

--- group_20_health_tracker_web/settings.py
+++ group_20_health_tracker_web/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3w(5roetr)+9-(96x%3=cg31p5$1+j89^+o-1u4jk4d5(mep!x'
+SECRET_KEY = '********************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -111,8 +111,8 @@ AUTH_PASSWORD_VALIDATORS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 MAILER_EMAIL_BACKEND = EMAIL_BACKEND
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_PASSWORD = 'oipzacknnvttqbpc'
-EMAIL_HOST_USER = 'healthtrackerteam@gmail.com'
+EMAIL_HOST_PASSWORD = '********************'
+EMAIL_HOST_USER = '********************'
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
